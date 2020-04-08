63560143e4107ba5281616243b1396946c77c515
buvoli <buvoli@uw.edu>
2020-04-07 18:27:58 -0700

    added local user theia install

--- build-scripts/user_install.sh
+++ build-scripts/user_install.sh
@@ -21,7 +21,8 @@
 # ---- Dev Environemnts --------------------------------------------------------
 #     DEV_JUPYTER     TRUE => Jupyter Notebook And Jupyter Lab with support for
 #                             all select languages.
-#
+#     DEV_THEIA       TRUE -> Theia IDE with support for selected languages.
+#     DEV_CLI         TRUE => CLI development tools: git, tmux, vim, emac
 # ---- Package Managers --------------------------------------------------------
 #     PKGM_SPACK      TRUE => Spack
 #
@@ -58,4 +59,38 @@ if [ "$PKGM_SPACK" = "TRUE" ] ; then
   mkdir -p ~/.local/bin
   git clone https://github.com/spack/spack.git ~/.local/spack
   ln -s ~/.local/spack/bin/spack ~/.local/bin/spack
+fi
+
+# -----> Theia
+if [ "$DEV_THEIA" = "TRUE" ] ; then
+  THEIA_INSTALL_DIR=~/.local/lib/theia
+  THEIA_PATH_DIR=~/.local/bin
+  PLUGIN_BUILD_DIR=~/.local/tmp/vs-plugins
+  # --> install nvm
+  curl --silent -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+  source ~/.bash_profile
+  # --> install latest node 10 (dubnium)
+  nvm install lts/dubnium
+  nvm use lts/dubnium
+  # --> install theia
+  alias python=python2
+  mkdir -p $THEIA_INSTALL_DIR
+  cd $THEIA_INSTALL_DIR
+  wget https://github.com/container-job-runner/stack-config-files/releases/download/0.1-alpha/package.json # get package.json file
+  yarn
+  yarn build
+  unalias python
+  # --> make a shell script for launching theia
+  mkdir -p $THEIA_PATH_DIR
+  echo -e '#!/bin/bash\nyarn --cwd '$(pwd)' start $@' >> "$THEIA_PATH_DIR/theia"
+  chmod a+x "$THEIA_PATH_DIR/theia"
+  # --> add additional plugins
+  npm install -g vsce # install visual code plugin
+  mkdir -p $PLUGIN_BUILD_DIR
+  # --> fortran plugin
+  cd $PLUGIN_BUILD_DIR
+  git clone https://github.com/krvajal/vscode-fortran-support.git --branch 2.1.0 --depth 1
+  cd "$PLUGIN_BUILD_DIR/vscode-fortran-support"
+  npm install && vsce package
+  cp linter-gfortran-2.1.0.vsix "$THEIA_INSTALL_DIR/plugins/linter-gfortran-2.1.0.vsix"
 fi
